3cf59da825d0e5839a420bf8ea94b8cf3e4ca02e
KrishnanditoLksn <krislaksana123@gmail.com>
2024-04-23 10:50:57 +0700

    feat : regis form styling

--- regis.php
+++ regis.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <style>
+    body {
+        background: url("intellij-2023.2-1920x1080.png");
+    }
+
     label,
     h1 {
         font-family: "Rubik", sans-serif;
-
+        color: green;
     }
 
     .container {
@@ -16,11 +20,44 @@
 
     .form-container {
         padding: 50px;
-        background-color: aqua;
+        background-color: aliceblue;
+        border-radius: 10px;
+        margin-top: 130px;
     }
 
     .regis_forms {
-        padding: 25px;
+        padding: 20px;
+        margin: 20px;
+    }
+
+    .regis_input {
+        margin-left: 40px;
+        padding-top: 35px;
+        margin-right: 45px;
+    }
+
+    input[type="text"],
+    input[type="password"] {
+        width: 120%;
+        padding: 10px 15px;
+        margin: 10px 0;
+        box-sizing: border-box;
+    }
+
+    .submit-regis {
+        margin-top: 40px;
+        margin-left: 62px;
+        box-shadow: 0 4px #c1a23c;
+        color: #5e4800;
+        background-color: white;
+        text-transform: uppercase;
+        padding: 10px 20px 20px;
+        border-radius: 2px;
+        margin-bottom: 20px;
+        transition: all .2s ease;
+        font-weight: 900;
+        cursor: pointer;
+        letter-spacing: 1px;
     }
 </style>
 
@@ -39,15 +76,16 @@
         <div class="form-container">
             <h1>Registrasi User Freak</h1>
             <form method="post" class="regis_forms">
-                <label for="nama">
-                    Nama
-                </label>
-                <input type="text" name="name">
-                <br>
-                <label for="password">Password</label>
-                <input type="password" name="password">
+                <div class="regis_input">
+                    <label for="fname">Nama Lengkap:</label><br>
+                    <input type="text" id="fname" name="fname"><br>
+                    <label for="password">Password:</label><br>
+                    <input type="password" id="password" name="password"><br><br>
+                </div>
+                <input type="submit" class="submit-regis" value="Registrasi">
             </form>
         </div>
+    </div>
     </div>
 </body>
 
